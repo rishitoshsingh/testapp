53d46d6f6d8197568f8a692516e18685fc0f234a
rishitoshsingh <rishitoshs@gmail.com>
2019-05-07 12:20:44 +0530

    Camera and UI Fixed

---
+++ app/src/main/java/com/alphae/testapp/CameraActivity.kt
@@ -0,0 +1,126 @@
+package com.alphae.testapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.camerakit.CameraKitView
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import id.zelory.compressor.Compressor
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_camera.*
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+
+class CameraActivity : AppCompatActivity() {
+
+    private var cameraKitView: CameraKitView? = null
+    private val TAG: String = "CameraActivity"
+    val UPLOADED: Int = 201
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+        cameraKitView = findViewById(R.id.camera)
+
+
+        camera_button.setOnClickListener {
+
+            cameraKitView?.captureImage(CameraKitView.ImageCallback { cameraKitView, capturedImage ->
+                val savedPhoto = File(Environment.getExternalStorageDirectory(), "photo.jpg")
+                try {
+                    val outputStream = FileOutputStream(savedPhoto.path)
+                    outputStream.write(capturedImage)
+                    outputStream.close()
+
+                    Compressor(this)
+                            .compressToFileAsFlowable(savedPhoto)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe({ file ->
+                                val len = savedPhoto.length() - file.length()
+                                Log.d(TAG, "actual Size ${savedPhoto.length()}")
+                                Log.d(TAG, "compressed Size ${file.length()}")
+
+                                upload(file)
+
+//                                Toast.makeText(this@CameraActivity, "Compressed $len",Toast.LENGTH_LONG).show()
+                            }, { throwable ->
+                                throwable.printStackTrace()
+                                Log.e(TAG, throwable.message)
+                            })
+
+
+                } catch (e: java.io.IOException) {
+                    e.printStackTrace()
+                }
+            })
+
+        }
+
+    }
+
+    private fun upload(file: File?) {
+        Toast.makeText(this, "Uploading", Toast.LENGTH_LONG).show()
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference
+        val imagesRef = storageRef.child("images/")
+        val stream = FileInputStream(file)
+        val uploadTask = imagesRef.putStream(stream)
+        val urlTask = uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+            if (!task.isSuccessful) {
+                task.exception?.let {
+                    throw it
+                }
+                finish()
+            }
+            return@Continuation imagesRef.downloadUrl
+        }).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val intent = Intent()
+                intent.putExtra("uploadedUrl", task.result.toString())
+                setResult(MainActivity().SELFIE_UPLOADED, intent)
+                finish()
+            } else {
+                // Handle failures
+                // ...
+            }
+        }.addOnFailureListener { exception -> Log.d(TAG, "Failed Upload", exception) }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        cameraKitView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        cameraKitView?.onResume()
+    }
+
+    override fun onPause() {
+        cameraKitView?.onPause()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        cameraKitView?.onStop()
+        super.onStop()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        cameraKitView?.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+}
--- app/src/main/java/com/alphae/testapp/MainActivity.kt
+++ app/src/main/java/com/alphae/testapp/MainActivity.kt
@@ -1,139 +1,128 @@
 package com.alphae.testapp
 
-import android.graphics.Color
-import android.graphics.PorterDuff
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.TextView
-import com.google.firebase.FirebaseException
-import com.google.firebase.FirebaseTooManyRequestsException
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.PhoneAuthCredential
-import com.google.firebase.auth.PhoneAuthProvider
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
 
-    private var numpad_state: Boolean = false
-    private var mBottomSheetBehaviour: BottomSheetBehavior<*>? = null
-    private val NUMBER_MESSAGE: String = "Enter Your Number"
-    private var numberCount = 0
     private val TAG: String = "MainActivity"
+    val SELFIE_UPLOADED: Int = 201
+
+    private var numpadState: Boolean = false
+    private var numberValidated = false
+    private var selfieUploaded = false
+
+    private var mBottomSheetBehaviour: BottomSheetBehavior<*>? = null
+    private lateinit var uploadedUrl: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val nestedScrollView = findViewById<View>(R.id.numpad_bottom_sheet)
-        mBottomSheetBehaviour = BottomSheetBehavior.from(nestedScrollView)
-
-        fab.setOnClickListener {
-            if (numberCount == 10) {
-                val ft: android.support.v4.app.FragmentTransaction = supportFragmentManager.beginTransaction()
-                val dialogFragment = CameraDialog()
-                dialogFragment.show(ft, "cameraDialog")
-
-            }
-            if (numpad_state) {
-                numpad_state = false
-                (mBottomSheetBehaviour as BottomSheetBehavior<*>?)?.state = BottomSheetBehavior.STATE_COLLAPSED
-                fab.setImageDrawable(getDrawable(R.drawable.ic_arrow_up))
-            } else {
-                numpad_state = true
-                (mBottomSheetBehaviour as BottomSheetBehavior<*>?)?.state = BottomSheetBehavior.STATE_EXPANDED
-                fab.setImageDrawable(getDrawable(R.drawable.ic_arrow_down))
-            }
-        }
-
-        number_text_view.setOnClickListener {
-            if (numpad_state) {
-                numpad_state = false
-                (mBottomSheetBehaviour as BottomSheetBehavior<*>?)?.state = BottomSheetBehavior.STATE_COLLAPSED
-                fab.setImageDrawable(getDrawable(R.drawable.ic_arrow_up))
-            } else {
-                numpad_state = true
-                (mBottomSheetBehaviour as BottomSheetBehavior<*>?)?.state = BottomSheetBehavior.STATE_EXPANDED
-                fab.setImageDrawable(getDrawable(R.drawable.ic_arrow_down))
-            }
-        }
-
+        viewInitializations()
+        listnersInitializations()
     }
 
     fun numberClicked(view: View) {
-        if (number_text_view.text == NUMBER_MESSAGE) number_text_view.text = ""
-        if (numberCount != 10) {
+        if (number_text_view.isFocused) {
             val textView = view as TextView
-            number_text_view.text = number_text_view.text.toString() + textView.text
-            numberCount++
+            number_text_view.text.append(textView.text)
         }
-
-        if (numberCount == 10) {
-            val fabDrawable = resources.getDrawable(R.drawable.ic_check)
-            val newFabIcon = fabDrawable.constantState!!.newDrawable()
-            newFabIcon.mutate().setColorFilter(Color.GREEN, PorterDuff.Mode.MULTIPLY)
-            fab.setImageDrawable(newFabIcon)
-        }
-
     }
 
     fun clearClicked(view: View) {
-        if (numberCount != 0) {
-            val textView = view as TextView
-            val number = number_text_view.text.toString()
-            number_text_view.text = number.substring(0, number.length - 1)
-            numberCount--
-            val fabDrawable = resources.getDrawable(R.drawable.ic_arrow_down)
-            val newFabIcon = fabDrawable.constantState!!.newDrawable()
-            newFabIcon.mutate().setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY)
-            fab.setImageDrawable(newFabIcon)
+        if (number_text_view.isFocused) {
+            val length = number_text_view.text.length
+            number_text_view.text.delete(length - 1, length)
         }
-        if (numberCount == 0) number_text_view.text = NUMBER_MESSAGE
     }
 
+    private fun viewInitializations() {
+        number_text_view.showSoftInputOnFocus = false
+        take_selfie.requestFocus()
+        val nestedScrollView = findViewById<View>(R.id.numpad_bottom_sheet)
+        mBottomSheetBehaviour = BottomSheetBehavior.from(nestedScrollView)
+    }
 
-//    private fun login() {
-//
-//        val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-//
-//            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-//                Log.d(TAG, "onVerificationCompleted:$credential")
-//                signInWithPhoneAuthCredential(credential)
-//                val credential = PhoneAuthProvider.getCredential(verificationId!!, code)
-//            }
-//
-//            override fun onVerificationFailed(e: FirebaseException) {
-//                Log.w(TAG, "onVerificationFailed", e)
-//                if (e is FirebaseAuthInvalidCredentialsException) {
-//                } else if (e is FirebaseTooManyRequestsException) {
-//                }
-//            }
-//
-//            override fun onCodeSent(
-//                    verificationId: String?,
-//                    token: PhoneAuthProvider.ForceResendingToken
-//            ) {
-//                Log.d(TAG, "onCodeSent:" + verificationId!!)
-//                storedVerificationId = verificationId
-//                resendToken = token
-//
-//            }
-//        }
-//
-//
-//        val phoneNumber = number_text_view.text.toString()
-//        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-//                phoneNumber,      // Phone number to verify
-//                60,               // Timeout duration
-//                TimeUnit.SECONDS, // Unit of timeout
-//                this,             // Activity (for callback binding)
-//                callbacks) // OnVerificationStateChangedCallbacks
-//
-//
-//    }
+    override fun onBackPressed() {
+        if (numpadState) {
+            number_text_view.isFocusable = false
+            number_text_view.isFocusableInTouchMode = true
 
+            numpadState = false
+            mBottomSheetBehaviour?.state = BottomSheetBehavior.STATE_COLLAPSED
+            fab.setImageDrawable(getDrawable(R.drawable.ic_arrow_up))
+        } else super.onBackPressed()
+    }
+
+    private fun listnersInitializations() {
+        fab.setOnClickListener {
+            if (numpadState) {
+                numpadState = false
+                mBottomSheetBehaviour?.state = BottomSheetBehavior.STATE_COLLAPSED
+                fab.setImageDrawable(getDrawable(R.drawable.ic_arrow_up))
+            } else {
+                numpadState = true
+                mBottomSheetBehaviour?.state = BottomSheetBehavior.STATE_EXPANDED
+                fab.setImageDrawable(getDrawable(R.drawable.ic_arrow_down))
+            }
+        }
+
+        number_text_view.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                numpadState = true
+                mBottomSheetBehaviour?.state = BottomSheetBehavior.STATE_EXPANDED
+                fab.setImageDrawable(getDrawable(R.drawable.ic_arrow_down))
+            } else {
+                numpadState = false
+                mBottomSheetBehaviour?.state = BottomSheetBehavior.STATE_COLLAPSED
+                fab.setImageDrawable(getDrawable(R.drawable.ic_arrow_up))
+            }
+        }
+
+        number_text_view.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val number = s.toString().length
+                if (number > 10) {
+                    s?.delete(number - 1, number)
+                }
+                numberValidated = number == 10
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        take_selfie.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivityForResult(intent, SELFIE_UPLOADED)
+        }
+
+        login_button.setOnClickListener {
+            when {
+                numberValidated -> Toast.makeText(this, "Check Your Number", Toast.LENGTH_LONG).show()
+                selfieUploaded -> Toast.makeText(this, "Please Take your Selfie", Toast.LENGTH_LONG).show()
+                else -> Toast.makeText(this, "Login", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == SELFIE_UPLOADED) {
+            selfieUploaded = true
+            if (data != null) {
+                uploadedUrl = data.getStringExtra("uploadedUrl")
+            }
+        } else Toast.makeText(this, "Error during Taking Selfie", Toast.LENGTH_LONG).show()
+    }
 }
